dd4b3483ebf264823e6bdff19175369ce82bd573
Sofia.R <soficastaeda0317@gmail.com>
2025-08-27 20:27:58 -0500

    Lagartija

---
+++ src/main/java/com/mycompany/lagartidasinfondo/Alimento.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lagartidasinfondo;
+
+/**
+ *
+ * @author sofia_yk3bfn
+ */
+public class Alimento {
+    private String Tipo_Alimento;
+    private int Unidad;
+
+    public Alimento(String Tipo_Alimento, int Unidad) {
+        this.Tipo_Alimento = Tipo_Alimento;
+        this.Unidad = Unidad;
+    }
+}
---
+++ src/main/java/com/mycompany/lagartidasinfondo/LagartidaSinFondo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lagartidasinfondo;
+
+/**
+ *
+ * @author sofia_yk3bfn
+ */
+
+public class LagartidaSinFondo {
+    
+    public static void main(String[] args) {
+        Alimento A1 = Alimmento new 
+    }
+}
